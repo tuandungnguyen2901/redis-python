7eb676c039cccfe646809f4c27527c6c99847242
dungnt <john.nguyen@politetech.com>
2024-03-22 23:36:09 +0700

    pass my 4st stage

--- app/main.py
+++ app/main.py
@@ -1,20 +1,27 @@
 # Uncomment this to pass the first stage
 import socket
+import asyncio
 
+async def handle_client(reader, writer):
+    pong = b"+PONG\r\n"
+    addr = writer.get_extra_info("peername")
+    print("Connected", addr)
+    while True:
+        data = await reader.read(1024)
+        if not data:
+            break
+        print("Data:", data.decode())
+        writer.write(pong)
+        await writer.drain()
+    print("Client disconnected")
+    writer.close()
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, address = server_socket.accept() # wait for client
-    while connection:
-        request = connection.recv(1024)
-        response = b"+PONG\r\n"
-        connection.sendall(response)
+async def main():
+    server = await asyncio.start_server(handle_client, "localhost", 6379)
+    print("Server listening on port 6379...")
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
