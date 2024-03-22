7b32d63cb93e849fd378713714cbedc1964268ed
dungnt <john.nguyen@politetech.com>
2024-03-22 23:19:37 +0700

    pass my 3rd stage

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
-    with connection:
+    while connection:
         request = connection.recv(1024)
         response = b"+PONG\r\n"
         connection.sendall(response)
