b8a24001cc82a9f860d68afc579d73317965197c
dungnt <john.nguyen@politetech.com>
2024-03-22 23:12:35 +0700

    pass my 2nd stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,11 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, address = server_socket.accept() # wait for client
+    with connection:
+        request = connection.recv(1024)
+        response = b"+PONG\r\n"
+        connection.sendall(response)
 
 
 if __name__ == "__main__":
